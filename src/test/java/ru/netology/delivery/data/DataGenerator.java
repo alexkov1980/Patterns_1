7f42a8332d19e3ca99c3d33bd37cc71f81e604e7
Alexey Kovalev <amkovalyov@yandex.ru>
2023-05-21 09:40:55 +0300

    Backspace

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -2,7 +2,6 @@ package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
-import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -16,15 +15,11 @@ public class DataGenerator {
     }
 
     public static String generateDate(int days) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-        String date  = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         return date;
     }
 
     public static String generateCity() {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
         List<String> list = Arrays.asList("Москва", "Санкт-Петербург", "Казань", "Калининград", "Ростов-на-Дону", "Краснодар", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск",
                 "Ульяновск");
         Random rand = new Random();
@@ -33,15 +28,11 @@ public class DataGenerator {
     }
 
     public static String generateName() {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
         Faker faker = new Faker(new Locale("ru"));
         return faker.name().firstName() + " " + faker.name().lastName();
     }
 
     public static String generatePhone() {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
         Faker faker = new Faker(new Locale("ru"));
         return faker.phoneNumber().phoneNumber();
     }
@@ -50,10 +41,8 @@ public class DataGenerator {
         private Registration() {
         }
 
-        public static UserInfo generateUser() {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
-            return new UserInfo(generateCity(),generateName(),generatePhone());
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(), generatePhone());
         }
     }
 
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
@@ -27,13 +28,17 @@ class DeliveryTest {
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         $("[data-test-id=city] .input__control").setValue(validUser.getCity());
-        $("[data-test-id=date] .input__control").doubleClick().sendKeys(firstMeetingDate);
+        $("[data-test-id=date] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(firstMeetingDate);
         $("[data-test-id=name] .input__control").setValue(validUser.getName());
         $("[data-test-id=phone] .input__control").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
         $("[data-test-id=success-notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("div.notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id=success-notification] .icon-button__text").click();
+        $("[data-test-id=date] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(secondMeetingDate);
         $(".button__content").click();
         $("[data-test-id=replan-notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("[data-test-id=replan-notification] .notification__content").shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
